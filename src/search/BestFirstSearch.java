4dd3822a7787ead4538007d92fec6712091be297
afstillo <57636631+afstillo@users.noreply.github.com>
2020-02-26 17:49:16 -0500

    Update BestFirstSearch.java

--- src/search/BestFirstSearch.java
+++ src/search/BestFirstSearch.java
@@ -1,23 +1,61 @@
-
-// Java code to illustrate add()
+//TODO: figure out how to implement heuristics and expand nodes based on heuristic value. Implement node class//
 import java.util.*;
 
 public class BestFirstSearch {
-    public Node BestFirstSearch(Graph g, Node start, Node goal) {
-        PriorityQueue<Node> queue = new PriorityQueue<Node>();
+    public static String BestFirstSearch(Graph g, int start, int goal) {
+        int[] parent = new int[g.getNumberOfVertices()];
+        boolean[] expanded = new boolean[g.getNumberOfVertices()];
+
+        int[][] A = g.getAdjacencyMatrix();
+        for (int i = 0; i<g.getNumberOfVertices(); i++)
+        {
+            parent[i] = 0;
+        }
+        parent[start] = -1;
+        String result = null;
+
+        expanded[start] = true;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
         //TODO: add comparator
         queue.add(start);
         while (!queue.isEmpty())
         {
-            Node u = queue.poll();
+            int u = queue.poll();
             if (u == goal) {
-                return u;
+                result = "Found: " + String.valueOf(u);
+                break;
             }
             else
             {
-
+                for(int v = 0; v < g.getNumberOfVertices(); v++)
+                {
+                    if (A[u][v] != 0 && expanded[v] == false)
+                    {
+                        queue.add(v);
+                        parent[v] = u;
+                    }
+                    expanded[u] = true;
+                }
             }
         }
-        return null;
+        if (result!=null)
+        {
+            //for finding path taken and total cost//
+            Stack<Integer> stack = new Stack<Integer>();
+            String path = String.valueOf(goal);
+            int cost = 0;
+
+            int i = goal;
+
+            while (i!=-1)
+            {
+                cost+=A[parent[i]][i];
+                i = parent[i];
+                path = String.valueOf(i) + " -> " + path;
+            }
+            result += "\nPath taken: " + path + "\n Total cost: " + cost;
+        }
+        return result;
     }
 }
